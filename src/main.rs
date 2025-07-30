172e4f8447374a836c0b91ce6ca82ed43d093973
Yorye <jorge.abrodes@gmail.com>
2025-07-30 22:15:09 +0200

    Preubas

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,20 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
-#[command(name = "life")]
-#[command(about = "TODO")]
-#[command(version, about, long_about = None)]
+#[command(name = "life", version, about = "CLI estilo life.txt")]
 
 struct Cli {
     #[command(subcommand)]
     command: Commands,
-
 }
-#[command(subcommand)]
-enum Commands{
-    Add {},
+
+#[derive(Subcommand)]
+enum Commands {
+    Add { task: String },
     List,
-    Done{},
-    Remove{}
 }
 
-struct Args {
-    /// Name of the person to greet
-    #[arg(short, long)]
-    name: String,
-
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
-}
 
 fn main() {
-    let args = Args::parse();
-
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name);
-    }
+    print!("Vivo?")
 }
